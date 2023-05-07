2d7f209f9dcd9d4b0baa0870f37e7c9448b60626
dark-king-001 <piyushat115@gmail.com>
2023-05-07 13:02:32 +0530

    example parser code

--- main.cpp
+++ main.cpp
@@ -4,20 +4,29 @@ using namespace std;
 #include "src/State.hpp"
 int main() {
     // Create the states
-    state s1, s2, s3, s4, s5;
+    state s1, s2, s3, s4, s5, s6, s7, s8, s9;
     
     // Add transitions between states
-    s1.addState('p', &s2);
-    s2.addState('r', &s3);
-    s3.addState('i', &s4);
-    s4.addState('n', &s5);
-    s5.addState('t', NULL);
-    s5.writeChar = "echo";
+    s1.addState('<', &s2);
+    s2.addState('a', &s3);
+
+    s2.addState('/', &s6);
+
+    s3.addState('p', &s4);
+    s4.addState('p', &s5);
+    s5.addState('>', &s1);
+
+    s6.addState('a', &s7);
+    s7.addState('p', &s8);
+    s8.addState('p', &s9);
+    s9.addState('>', NULL);
+
+    s5.writeChar = " bit-stream-true \n";
+    s9.writeChar = " bit-stream-false \n";
     
     // Test the DFA
-    string input = "print";
+    string input = "<app></app>";
     cout << "Input: " << input << endl;
-    cout << "Output: ";
     s1.transition(&s1, input);
     
     return 0;
